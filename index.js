3423aaf6c833122cb53549117098be108c36b914
sebastian medina <sebastianlopez_21@hotmail.com>
2022-07-18 11:05:07 -0400

    commit tree 1c2e4334278510fd052d13b31742aec48bb6a5e8

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}*/
+
+const names =["Guadalupe", "Ollie", "Aki"];
+
+const messages =[];
+
+function writeCards(names, evenName){
+
+    for(let i = 0; i< names.length; i++ ){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${evenName} gift!`);
+        
+    }
+
+    return messages;
+}
+
+//writeCards(names,"surprise");
+console.log(messages);
+
+function countDown(){
+
+    let i = 10;
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+}
+
+countDown();
